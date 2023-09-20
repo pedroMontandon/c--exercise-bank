0167a3e98676ccf335f428f756d432d1d3adb581
pedroMontandon <pedrobarbosa89@gmail.com>
2023-09-20 15:23:19 -0300

    the checkBalance method has been implemented

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -79,7 +79,8 @@ public class Trybank
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();   
+        if(!Logged) throw new AccessViolationException("Usuário não está logado");
+        return Bank[loggedUser, 3];
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
